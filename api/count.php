df991a0e3331bc48aafa1086c1344efc3c04230a
Jarvis Zhang <345054597@qq.com>
2015-12-26 17:50:06 +0800

    Alpha 1.0.0.6
    修复2处安全漏洞

--- api/count.php
+++ api/count.php
@@ -14,8 +14,24 @@ else{
 		$id=$_GET['id'];
 		$DB_hitsid.='-'.$id;
 	}
+	$DB_hitsid=htmlspecialchars($DB_hitsid,ENT_QUOTES);
+	isSQL($DB_hitsid);
 	$DB_mysql->query("SELECT * FROM `cms_hits` WHERE hitsid='".$DB_hitsid."'");
 	$DB_row=$DB_mysql->result->fetch_assoc();
 	$DB_hits=$DB_row["hits"];
+}
+function isSQL($str){
+	$forbid=array('=','/','&','(',')','?','*','#','@','+','{','}','|','"',"'");
+	$a=0;
+	for($a=0;$a<count($forbid);$a++){
+		if(strpos($str,$forbid[$a])>-1){
+			$jsonArr=array(
+				'code'=>'0',
+				'msg'=>'heheda :)'
+			);
+			$jsonData=json_encode($jsonArr);
+			die($jsonData);
+		}
+	}
 }
 echo "document.getElementById('hits-".$DB_hitsid."').innerHTML='".$DB_hits."';";
--- api/statistic.php
+++ api/statistic.php
@@ -4,6 +4,22 @@
  * Date: 2015/10/26
  */
 $hitsid=@$_GET['hitsid'];
+$hitsid=htmlspecialchars($hitsid,ENT_QUOTES);
+isSQL($hitsid);
+function isSQL($str){
+	$forbid=array('=','/','&','(',')','?','*','#','@','+','{','}','|','"',"'");
+	$a=0;
+	for($a=0;$a<count($forbid);$a++){
+		if(strpos($str,$forbid[$a])>-1){
+			$jsonArr=array(
+				'code'=>'0',
+				'msg'=>'heheda :)'
+			);
+			$jsonData=json_encode($jsonArr);
+			die($jsonData);
+		}
+	}
+}
 $DB_mysql=new database();
 $sql="UPDATE `cms_hits` SET `hits`=`hits`+1  WHERE `hitsid`='".$hitsid."'";
 $DB_mysql->query($sql);
