08ac5c20b529ce83db108b28745745966322a511
Jarvis Zhang <345054597@qq.com>
2015-12-26 21:52:33 +0800

    Alpha 1.0.0.6

--- libs/config.php
+++ libs/config.php
@@ -5,7 +5,7 @@
  * Date: 2015/9/25
  * Time: 8:26
  */
-define("CMS_VERSON","Alpha 1.0.0.4");
+define("CMS_VERSON","Alpha 1.0.0.6");
 define("PATH_CACHE","/cache/");
 define("PATH_TEMPLATES","/templates/");
 define("FILE_DATACONFIG","/lib/database.php");
